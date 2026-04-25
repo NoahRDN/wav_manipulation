e7af444c7b0b38b4947645834a5a908a1e9e0e82
Noah <noahmichaelrandrianah@gmail.com>
2026-04-25 07:03:58 +0300

    Lecture de l'en-tête

--- main.cpp
+++ main.cpp
@@ -3,10 +3,19 @@
 #include <vector>
 #include <cstdint>
 
+uint16_t readUInt16LE(const std::vector<uint8_t>& buffer, size_t offset) {
+    return buffer[offset] | (buffer[offset + 1] << 8);
+}
 
+uint32_t readUInt32LE(const std::vector<uint8_t>& buffer, size_t offset) {
+    return buffer[offset]
+         | (buffer[offset + 1] << 8)
+         | (buffer[offset + 2] << 16)
+         | (buffer[offset + 3] << 24);
+}
 
 int main() {
-    std::ifstream file("input.wav", std::ios::binary);
+    std::ifstream file{"input.wav", std::ios::binary};
 
     if (!file) {
         std::cerr << "Erreur : impossible d'ouvrir input.wav\n";
@@ -19,6 +28,42 @@ int main() {
     };
 
     std::cout << "Fichier charge : " << buffer.size() << " octets\n";
+
+
+    for (int i = 22; i < 24; i++) {
+        printf("0x%02X ", buffer[i]);
+    }
+    
+    printf("\n");
+
+    for (int i = 22; i < 24; i++) {
+        std::cout << (char)buffer[i];
+    }
+    
+    std::cout << "\n";
+    printf("%ld\n", buffer.size());
+    
+    // -----------------------------------------------------
+
+    if (buffer[0] != 'R' || buffer[1] != 'I' || buffer[2] != 'F' || buffer[3] != 'F') {
+        std::cerr << "Pas un fichier RIFF\n";
+        return 1;
+    }
+
+    if (buffer[8] != 'W' || buffer[9] != 'A' || buffer[10] != 'V' || buffer[11] != 'E') {
+        std::cerr << "Pas un fichier WAVE\n";
+        return 1;
+    }
+
+    // -----------------------------
+
+    uint16_t numChannels   = readUInt16LE(buffer, 22);
+    uint32_t sampleRate    = readUInt32LE(buffer, 24);
+    uint16_t bitsPerSample = readUInt16LE(buffer, 34);
+
+    std::cout << "Canaux : " << numChannels << "\n";
+    std::cout << "Frequence : " << sampleRate << " Hz\n";
+    std::cout << "Bits par echantillon : " << bitsPerSample << "\n";
 
     return 0;
 }
